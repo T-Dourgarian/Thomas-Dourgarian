828c6c6bce77c658492639aa6a47e6b82890b1a2
T-Dourgarian <thomasdourgarian@gmail.com>
2020-11-02 15:50:11 -0600

    add bitcoin bot app to cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,18 +12,26 @@ class Projects extends Component {
             <div id='projects' className='Projects'>
                 <h1 className="projectsTitle" >PROJECTS</h1>
                 <div style={{ display: 'flex' }}>
-                    <ProjectCard
-                        technologies={['JAVASCRIPT', 'REACT', 'NODE', 'POSTGRESQL', 'TWILIO', 'GOOGLE MAPS API']}
-                        description={`WALLET WIZARD IS A FULL-STACK WEB APPLICATION THAT ALLOWS USERS TO MANAGE COUPONS AND SET REMINDERS FOR THEM ALL VIA SMS TEXT MESSAGING. WALLET WIZARD ALSO UTILIZES GOOGLE MAPS API TO SHOW USERS NEARBY LOCATIONS THAT THEY HAVE COUPONS FOR AND THEIR CURRENT DISTANCE FROM THOSE PLACES.`}
-                        link={`https://wallet-wizard.herokuapp.com/#/home`}
-                        title='WALLET WIZARD'
-                    />
+					
+					<ProjectCard
+						technologies={['JAVASCRIPT', 'VUE', 'NODE', 'MONGODB', 'BINANCE API']}
+						description={`A BITCOIN TRADING BOT WITH A CUSTOM ANALYSIS INTERFACE THAT UTILIZES BINANCE'S PUBLIC TRADING API. IT UTILIZES COMMON TRADING INDICATOR VALUES, CALCULATED USING BITCOIN PRICE HISTORY, TO DETERMINE OPTIMAL BUY AND SELL PRICES`}
+						link={`https://td-binance.herokuapp.com/#/login`}
+						title='BITCOIN TRADING BOT'
+					/>
 
                     <ProjectCard
                         technologies={['JAVASCRIPT', 'REACT', 'NODE', 'POSTGRESQL', 'AUTO ML']}
                         description={`SIID (SOCIAL IMPACT IDENTIFICATION) IS A FULL-STACK WEB APPLICATION THAT LEVERAGES A RULES-BASED FLAGGING SYSTEM AND MACHINE LEARNING TO DETECT AND PREVENT NEGATIVE SOCIAL IMPACT IN MARKETING CAMPAIGNS WHILE ALSO CONNECTING MARKETERS TO EXPERTS IN IDENTIFIED AREAS OF SENSITIVITY.`}
                         link={`http://siid-tool.herokuapp.com/#/home`}
                         title='SIID'
+                    />
+
+                    <ProjectCard
+                        technologies={['JAVASCRIPT', 'REACT', 'NODE', 'POSTGRESQL', 'TWILIO', 'GOOGLE MAPS API']}
+                        description={`WALLET WIZARD IS A FULL-STACK WEB APPLICATION THAT ALLOWS USERS TO MANAGE COUPONS AND SET REMINDERS FOR THEM ALL VIA SMS TEXT MESSAGING. WALLET WIZARD ALSO UTILIZES GOOGLE MAPS API TO SHOW USERS NEARBY LOCATIONS THAT THEY HAVE COUPONS FOR AND THEIR CURRENT DISTANCE FROM THOSE PLACES.`}
+                        link={`https://wallet-wizard.herokuapp.com/#/home`}
+                        title='WALLET WIZARD'
                     />
 
                     {/* <ProjectCard /> */}
